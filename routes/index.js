749eb9750899a944dee63b371a6add2efea258ec
B-Musick <bmuze1@gmail.com>
2019-10-10 20:44:19 -0600

    Added passport authorization routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 let express = require('express'),
-    router  = express.Router();
+    router  = express.Router(),
+    passport = require('passport');
+    
+let User = require('../models/user');
 
 router.get('/',(req, res)=>{
     res.render('landing');
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-let express = require('express'),
-    app     = express();
+let express  = require('express'),
+    app      = express(),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local');
+
+/***************************** MODELS  ****************************************/ 
+let User = require('./models/user');
+
+/**************************PASSPORT CONFIGURATION *************************** */
+app.use(require('express-session')({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// TELL EXPRESS APP TO USE PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+
+// AUTHENTICATE SOMEONE LOCALLY
+passport.use(new LocalStrategy(User.authenticate()));
+
+/* To serialize means converting objects contents into a small key that can then 
+be deserialized into the original object. This allows us to know whos communicated 
+with the server without having to send the authentication data like username and 
+password at each request for a new page */
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 let indexRoutes = require('./routes/index.js');
